3f020794f239dcf17eff4ee6309283e0415a7125
alexandimis <alexandimis@gmail.com>
2026-07-08 02:03:05 +0300

    Added a send_data function so the players data can be sent to the server for proccessing

--- source/game/client.c
+++ source/game/client.c
@@ -5,11 +5,13 @@
 
 #include "client.h"
 
+int client_fd;
+struct sockaddr_un addr;
+
 // Connect to the server
 int server_connect_host(char *socket_path)
 {
-    struct sockaddr_un addr;
-    int client_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+    client_fd = socket(AF_UNIX, SOCK_STREAM, 0);
 
     addr.sun_family = AF_UNIX;
     strcpy(addr.sun_path, socket_path);
@@ -17,4 +19,22 @@ int server_connect_host(char *socket_path)
     int error = connect(client_fd, (struct sockaddr *)&addr, sizeof(addr));
 
     return error;
+}
+
+// Send the server data 
+int server_send_data(int data, char *magic_number)
+{
+    if (send(client_fd, magic_number, sizeof(magic_number), 0) == -1)
+    {
+        perror("sendto() failed to send data: ");
+        return -1;
+    }
+
+    if (send(client_fd, &data, sizeof(data), 0) == -1)
+    {
+        perror("sendto() failed to send data: ");
+        return -1;
+    }
+
+    return 0;
 }
--- source/game/client.h
+++ source/game/client.h
@@ -1,5 +1,8 @@
 #ifndef CLIENT_H
     #define CLIENT_H
 
+    #define PARTY_HOST_MAGIC_NUMBER "IAmYourHost"
+    #define PARTY_MEMBER_MAGIC_NUMBER "IAmNotYourHost"
     int server_connect_host(char *socket_path);
+    int server_send_data(int data, char *magic_number);
 #endif
