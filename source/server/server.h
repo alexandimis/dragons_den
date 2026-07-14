d22b3ef0d64de512025161c93f1e69a015c141cf
alexandimis <alexandimis@gmail.com>
2026-07-14 18:21:24 +0300

    Added a user_t and a list_t struct

--- source/server/server.h
+++ source/server/server.h
@@ -2,8 +2,20 @@
     #define SERVER_H
 
     #define MAX_PLAYERS 5
+    #define AFK_TIMEOUT 180
 
     typedef struct {
+        char name[NAME_SIZE];
         int user_fd;
+        int socket_fd;
     } user_t;
+
+    typedef struct list_t list_t;
+
+    struct list_t {
+        user_t user;
+        list_t *next;
+    };
+
+    bool ping(user_t user);
 #endif
