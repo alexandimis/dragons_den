9f5ac26194c49bdbbe11de17feb3832d2e8763b2
alexandimis <alexandimis@gmail.com>
2026-07-20 13:59:00 +0300

    Added declarations for add_user(), remove_user() and print_users()

--- source/server/server.h
+++ source/server/server.h
@@ -2,11 +2,11 @@
     #define SERVER_H
 
     #define MAX_PLAYERS 5
-    #define AFK_TIMEOUT 180
+    #define AFK_TIMEOUT 10
 
     typedef struct {
         char name[NAME_SIZE];
-        int user_fd;
+        int id;
         int socket_fd;
     } user_t;
 
@@ -14,8 +14,14 @@
 
     struct list_t {
         user_t user;
+        list_t *previous;
         list_t *next;
     };
 
+    // Functions
     bool ping(user_t user);
+    void add_user(user_t user);
+    void remove_user(user_t user);
+    list_t *find_user(int id);
+    void print_users();
 #endif
